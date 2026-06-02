51692720937828df1121f24286bfe56cfc63f73b
abderrahmanelaifa <abderrahmanelaifa3@gmail.com>
2026-06-02 11:35:03 +0100

    commit2

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-# Exécuter les migrations de la base de données
+
+# 1. Création des sous-dossiers indispensables s'ils manquent
+mkdir -p /var/www/html/storage/framework/cache/data
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/logs
+
+# 2. Ajustement des permissions pour Apache (www-data)
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+# 3. Exécuter les migrations de la base de données
 php artisan migrate --force
 
-# Optimiser les performances
+# 4. Optimiser les performances
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Lancer le serveur Apache
+# 5. Lancer le serveur Apache
 apache2-foreground
